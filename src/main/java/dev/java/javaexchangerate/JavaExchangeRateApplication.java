858b508b1ef711465415046bdc61cc527e59029c
10M <endlesslhk@gmail.com>
2022-06-02 14:18:10 +0900

    Java Exchange Rate Project Init

---
+++ src/main/java/dev/java/javaexchangerate/JavaExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package dev.java.javaexchangerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExchangeRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExchangeRateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/java/javaexchangerate/JavaExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.javaexchangerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExchangeRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
